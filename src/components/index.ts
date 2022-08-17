8bac52386c37ce2eaf814f838c202945bfd2df78
Cloakd <thecloakdone@gmail.com>
2022-08-17 07:35:13 +0100

    Bug fixes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,10 @@
+// @ts-ignore
 export {default as WalletConnectButton} from './WalletConnectButton.vue';
+// @ts-ignore
 export {default as WalletDisconnectButton} from './WalletDisconnectButton.vue';
+// @ts-ignore
 export {default as WalletIcon} from './WalletIcon.vue';
+// @ts-ignore
 export {default as WalletModalProvider} from './WalletModalProvider.vue';
+// @ts-ignore
 export {default as WalletMultiButton} from './WalletMultiButton.vue';
