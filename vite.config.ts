be8e4178a3d014621ffaebbb941794f83c6d52ea
Cloakd <thecloakdone@gmail.com>
2022-08-17 07:58:42 +0100

    Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { createVuePlugin } from 'vite-plugin-vue2'
 const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [createVuePlugin()],
 	resolve: {
 		dedupe: ['vue'],
 		alias: {
